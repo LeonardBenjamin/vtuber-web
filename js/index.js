84d3d240025ac7a917f78316e1dee3fbeb2b3ef8
Kai Aetherweigh <136946134+LeonardBenjamin@users.noreply.github.com>
2024-07-04 15:48:22 +0700

    .

--- js/index.js
+++ js/index.js
@@ -1,14 +1,9 @@
-fs.writeFile('talents.html', `<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HTML</title>
-</head>
-<body>
-    
-</body>
-</html>`, (err) => {
-    if (err) throw err;
-    console.log('File created successfully!');
-});
+let welcomeDiv = document.createElement('div');
+welcomeDiv.id = 'welcome';
+let welcomeH1 = document.createElement('h1');
+welcomeH1.textContent = 'Welcome to Oceanid Project!';
+welcomeDiv.appendChild(welcomeH1);
+document.body.appendChild(welcomeDiv);
+let welcomeH4 = document.createElement('h4');
+welcomeH4.textContent = 'Oceanid Project is a virtual YouTuber group. We are a group of talented people who come together to create and share our passion for music, art, and entertainment. Our goal is to bring joy and inspiration to our fans and to create a community where people can come together and share their creativity. We are always looking for new members to join our team and contribute their talents to our projects. If you are interested in joining us, please contact us through our website.';
+welcomeDiv.appendChild(welcomeH4);
