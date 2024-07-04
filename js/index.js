230bae6e0d4f0c4dc1c7af91867ff0f8a726ce6c
Kai Aetherweigh <136946134+LeonardBenjamin@users.noreply.github.com>
2024-07-04 19:36:42 +0700

    .

--- js/index.js
+++ js/index.js
@@ -7,3 +7,4 @@ document.body.appendChild(welcomeDiv);
 let welcomeH4 = document.createElement('h4');
 welcomeH4.textContent = 'Oceanid Project is a virtual YouTuber group. We are a group of talented people who come together to create and share our passion for music, art, and entertainment. Our goal is to bring joy and inspiration to our fans and to create a community where people can come together and share their creativity. We are always looking for new members to join our team and contribute their talents to our projects. If you are interested in joining us, please contact us through our website.';
 welcomeDiv.appendChild(welcomeH4);
+
